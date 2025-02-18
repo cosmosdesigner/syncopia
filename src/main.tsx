acb9ac57160c7a278d110a5d367b7dc7177514c5
Carlos Almeida <cosmosdesigner@gmail.com>
2025-02-18 22:12:44 +0000

    add hashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import CalendarView from './App.tsx';
-import HomePage from './pages/Home.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import CalendarView from "./App.tsx";
+import HomePage from "./pages/Home.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
       <Routes>
